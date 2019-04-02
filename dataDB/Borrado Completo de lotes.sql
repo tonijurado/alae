ae6d9c8abb80316801f36db6599b2d3a673de05d
Toni Jurado <tonijurado@cilantroit.com>
2019-04-02 11:21:07 +0200

    Cambio de script SQL de borrado de lotes

--- dataDB/Borrado Completo de lotes.sql
+++ dataDB/Borrado Completo de lotes.sql
@@ -1,4 +1,4 @@
-DELETE FROM alae_error WHERE 1; /* Borramos ALAE ERROR */
+DELETE FROM alae_error WHERE 1;
 DELETE FROM alae_sample_batch WHERE 1; /*Borramos ALAE SAMPLE BATCH*/
 DELETE FROM alae_batch WHERE 1; /*Borramos ALAE BATCH*/
 
