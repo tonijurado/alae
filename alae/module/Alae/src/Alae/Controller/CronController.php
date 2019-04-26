2a0471f2395216943a0d17e66e26dd9a5e9a6dc7
Toni Jurado <tonijurado@cilantroit.com>
2019-04-26 16:44:40 +0200

    Cambio del signo del tamaño del fichero minimo

--- alae/module/Alae/src/Alae/Controller/CronController.php
+++ alae/module/Alae/src/Alae/Controller/CronController.php
@@ -185,7 +185,7 @@ class CronController extends BaseController
         $fileSize = $data["size"];
         $Batch = $this->saveBatch($fileName, $fileSize);
         //si el tamaño del archivo es mayor que el tamaño del parametro
-        if($fileSize > $value)
+        if($fileSize < $value)
         {
             $this->execute(\Alae\Service\Verification::updateBatch("b.pkBatch = " . $Batch->getPkBatch(), "V1.1"));
         }
