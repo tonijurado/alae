020d982f63038097838618a1e6583a1fabe85e00
Toni Jurado <tonijurado@cilantroit.com>
2019-05-30 06:42:49 +0200

    Actualización de fotos

--- dataDB/Borrado Completo de lotes.sql
+++ dataDB/Borrado Completo de lotes.sql
@@ -1,5 +1,6 @@
 DELETE FROM alae_error WHERE 1;
 DELETE FROM alae_sample_batch WHERE 1; /*Borramos ALAE SAMPLE BATCH*/
+DELETE FROM alae_batch_nominal WHERE 1;
 DELETE FROM alae_batch WHERE 1; /*Borramos ALAE BATCH*/
 
 /*Borro el estudio a mano*/
