64d28db593bdd9f62728ca5499f22da8bc5a171d
Toni Jurado <tcilantro@ANAPHARMEUROPE>
2020-04-25 09:00:14 +0200

    Consolido en Head

--- dataDB/BorradoCompleto.sql
+++ dataDB/BorradoCompleto.sql
@@ -6,7 +6,7 @@ DELETE FROM alae_batch WHERE 1;
 -- Arriba borrado de lotes
 -- Abajo borrado completo
 
-DELETE FROM alae_sample_verification_study WHERE 1;
-DELETE FROM alae_analyte_study WHERE 1;
-DELETE FROM alae_study WHERE 1;
+-- DELETE FROM alae_sample_verification_study WHERE 1;
+-- DELETE FROM alae_analyte_study WHERE 1;
+-- DELETE FROM alae_study WHERE 1;
 
