b1cdf22fea7504c147a9072b8ff4ecbc18a0517d
Toni Jurado <tcilantro@ANAPHARMEUROPE>
2020-04-25 14:18:18 +0200

    Exportacion de BBDD Validaciones y ALAE2 correctas

---
+++ dataDB/old/BorradoCompleto.sql
@@ -0,0 +1,12 @@
+DELETE FROM alae_error WHERE 1;
+DELETE FROM alae_batch_nominal WHERE 1;
+DELETE FROM alae_sample_batch WHERE 1;
+DELETE FROM alae_batch WHERE 1;
+
+-- Arriba borrado de lotes
+-- Abajo borrado completo
+
+-- DELETE FROM alae_sample_verification_study WHERE 1;
+-- DELETE FROM alae_analyte_study WHERE 1;
+-- DELETE FROM alae_study WHERE 1;
+
--- dataDB/scripttest.bat
+++
@@ -1,2 +0,0 @@
-php C:\xampp\htdocs\alae2Test\alae\validaciones\public\index.php checkdirectory
-php C:\ xampp\htdocs\alae2Test\alae\alae\public\index.php checkdirectory
