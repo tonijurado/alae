c0af11a784aa7f081acadbef039fcbadb6bb0d95
Toni Jurado <tcilantro@ANAPHARMEUROPE>
2020-04-26 21:25:42 +0200

    Copia de vars opetativos de AE

---
+++ alae/module/Alae/config/Copia de vars de Alae2 operativa en AE.php
@@ -0,0 +1,33 @@
+<?php
+/* APLICACION ALAE 
+   Variables de control de rutas del sistema
+   Autor: María Quiroz
+   Fecha de creación: 10/05/2014
+*/
+namespace Alae;
+
+return array(
+    //RUTA BASE
+    "base_url"              => "http://www.test-alae.com/alae/alae/public",
+    "base_url_validation"   => "http://www.test-alae.com/alae/validaciones/public",
+ //"base_url" => "http://www.alaeapp.com",
+    //"base_url" => "http://localhost/alae/public",
+    "batch_directory"       => "D:\LotesAlae2Test",
+    "batch_directory_older" => "D:\LotesAlae2TestBKP",
+    "mail_host_smtp" 		=> "smtp.anapharmbioanalytics.com",
+    "mail_port" 			=> 587,
+	"mail_username"			=> "cgp579c",
+    "mail_admin_email" 		=> "alae@anapharmbioanalytics.com",
+    "mail_password" 		=> "nyCi5IE7",
+);
+
+/*return array(
+    "base_url" 				=> "http://www.alae.com/public",
+    "batch_directory" 		=> "D:\lotes",
+	"batch_directory_older" => "D:\lotesBKP",
+    "mail_host_smtp" 		=> "smtp.anapharmeurope.com",
+    "mail_port" 			=> 587,
+	"mail_username"			=> "alae@anapharmeurope.com",
+    "mail_admin_email" 		=> "alae@anapharmeurope.com",
+    "mail_password" 		=> "App2014",
+);*/
---
+++ validaciones/module/Alae/config/BKP de vars validaciones operativa en AE.php
@@ -0,0 +1,32 @@
+<?php
+/* APLICACION ALAE  
+   Variables de control de rutas del sistema
+   Autor: María Quiroz
+   Fecha de creación: 10/05/2014
+*/
+namespace Alae;
+
+return array(
+    //RUTA BASE
+    "base_url"              => "http://www.test-alae.com/alae/validaciones/public",
+ //"base_url" => "http://www.alaeapp.com",
+    //"base_url" => "http://localhost/alae/public",
+    "batch_directory"       => "D:\LotesValidacionesTest",
+    "batch_directory_older" => "D:\LotesValidacionesTestBKP",
+    "mail_host_smtp" 		=> "smtp.anapharmbioanalytics.com",
+    "mail_port" 			=> 587,
+	"mail_username"			=> "cgp579c",
+    "mail_admin_email" 		=> "alae@anapharmbioanalytics.com",
+    "mail_password" 		=> "nyCi5IE7",
+);
+
+/*return array(
+    "base_url" 				=> "http://www.alae.com/public",
+    "batch_directory" 		=> "D:\lotes",
+	"batch_directory_older" => "D:\lotesBKP",
+    "mail_host_smtp" 		=> "smtp.anapharmeurope.com",
+    "mail_port" 			=> 587,
+	"mail_username"			=> "alae@anapharmeurope.com",
+    "mail_admin_email" 		=> "alae@anapharmeurope.com",
+    "mail_password" 		=> "App2014",
+);*/
