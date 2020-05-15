9e2923d1c53111c447c2d55eb3f620a18e643ba2
Toni Jurado <tonijurado@cilantroit.com>
2020-05-15 11:23:09 +0200

    Ajustes para incidencia 27

--- validaciones/module/Alae/src/Alae/Controller/VerificationController.php
+++ validaciones/module/Alae/src/Alae/Controller/VerificationController.php
@@ -187,7 +187,7 @@ class VerificationController extends BaseController
                     {
                         $pkSampleBatch = $sampleBatchReason['pkSampleBatch'];
                         $parameters = $this->getRepository("\\Alae\\Entity\\Parameter")->findBy(array("rule" => $request->getPost('reason_'.$pkSampleBatch)));
-                        if ($request->getPost('reason_'.$pkSampleBatch) == "V12.8")
+                        if ($request->getPost('reason_'.$pkSampleBatch) == "V12.9")
                         {
                             $this->evaluation($Batch, false, $parameters[0]);
                         }
@@ -196,6 +196,18 @@ class VerificationController extends BaseController
                         AND s.pkSampleBatch = " . $pkSampleBatch . "
                         AND (
                                 ((s.sampleName LIKE 'CS1%' OR s.sampleName LIKE 'LLOQ%' OR s.sampleName LIKE 'LLQC%') AND s.accuracy NOT BETWEEN " . $min1  . " AND " . $max1 . " AND s.useRecord = 1)
+                                OR (s.sampleType LIKE 'Standard' AND s.sampleName NOT LIKE 'CS1%' AND s.accuracy NOT BETWEEN " . $min2 . " AND " . $max2 . " AND s.useRecord = 1)
+                                OR (s.sampleType LIKE 'Quality Control' AND s.sampleName NOT LIKE 'LLOQ%' AND s.sampleName NOT LIKE 'LLQC%' AND s.sampleName NOT LIKE 'TZ%' AND s.accuracy NOT BETWEEN " . $min3 . " AND " . $max3 . " AND s.useRecord = 1)
+                                OR (s.sampleName LIKE 'TZ%' AND s.accuracy NOT BETWEEN " . $min4 . " AND " . $max4 . " AND s.useRecord = 1)
+
+                                OR ((s.sampleName LIKE 'CS1%' OR s.sampleName LIKE 'LLOQ%' OR s.sampleName LIKE 'LLQC%') AND s.accuracy BETWEEN " . $min1  . " AND " . $max1 . " AND s.useRecord = 0)
+                                OR (s.sampleType LIKE 'Standard' AND s.sampleName NOT LIKE 'CS1%' AND s.accuracy BETWEEN " . $min2 . " AND " . $max2 . " AND s.useRecord = 0)
+                                OR (s.sampleType LIKE 'Quality Control' AND s.sampleName NOT LIKE 'LLOQ%' AND s.sampleName NOT LIKE 'LLQC%' AND s.sampleName NOT LIKE 'TZ%' AND s.accuracy BETWEEN " . $min3 . " AND " . $max3 . " AND s.useRecord = 0)
+                                OR (s.sampleName LIKE 'TZ%' AND s.accuracy BETWEEN " . $min4 . " AND " . $max4 . " AND s.useRecord = 0)
+                            )";
+
+
+/*
                                 OR (REGEXP(s.sampleName, :regexp1) = 1 AND s.sampleName NOT LIKE 'CS1%' AND s.accuracy NOT BETWEEN " . $min2 . " AND " . $max2 . " AND s.useRecord = 1)
                                 OR (REGEXP(s.sampleName, :regexp2) = 1 AND s.accuracy NOT BETWEEN " . $min3 . " AND " . $max3 . " AND s.useRecord = 1)
                                 OR (s.sampleName LIKE 'TZ%' AND s.accuracy NOT BETWEEN " . $min4 . " AND " . $max4 . " AND s.useRecord = 1)
@@ -209,8 +221,9 @@ class VerificationController extends BaseController
                                 OR (REGEXP(s.sampleName, :regexp3) = 1 AND s.accuracy BETWEEN " . $min5 . " AND " . $max5 . " AND s.useRecord = 0)
                                 OR (s.sampleName LIKE 'ULQC%' AND s.accuracy BETWEEN " . $min5 . " AND " . $max5 . " AND s.useRecord = 0)
                             )";
-
-                        $this->error($where, $parameters[0], array('regexp1' => '^CS[0-9]+(-[0-9]+)?$','regexp2' => '^QC[0-9]+(-[0-9]+)?$','regexp3' => '^((LD|HD|UL)?QC)[0-9]+(-[0-9]+)?$'), false);
+*/
+//                        $this->error($where, $parameters[0], array('regexp1' => '^CS[0-9]+(-[0-9]+)?$','regexp2' => '^QC[0-9]+(-[0-9]+)?$','regexp3' => '^((LD|HD|UL)?QC)[0-9]+(-[0-9]+)?$'), false);
+                        $this->error($where, $parameters[0]);
                         $this->V13_23($Batch);
                     }
                 //}
