eab115584eabd6a96098771237653a21f6865648
Toni Jurado <tonijurado@cilantroit.com>
2020-06-09 08:03:12 +0200

    Modificación Script de eliminacion completa de estudio

--- dataDB/ELIMINACION COMPLETA DE UN ESTUDIO.sql
+++ dataDB/ELIMINACION COMPLETA DE UN ESTUDIO.sql
@@ -2,27 +2,27 @@
 
  DELETE FROM alae_error 
  WHERE alae_error.fk_sample_batch IN 
- 	(SELECT pk_sample_batch FROM alae_sample_batch WHERE fk_batch IN (347, 348, 349, 350, 351, 352, 358, 359, 360, 361, 362, 363, 417, 425));
--- 		(
--- 			SELECT pk_batch FROM alae_batch WHERE alae_batch.fk_study IN (397)
--- 		)
--- 	);
+ 	(SELECT pk_sample_batch FROM alae_sample_batch WHERE fk_batch IN -- (347, 348, 349, 350, 351, 352, 358, 359, 360, 361, 362, 363, 417, 425));
+	 	(
+		 	SELECT pk_batch FROM alae_batch WHERE alae_batch.fk_study IN (403)
+ 		)
+ 	);
 -- 	
 -- Borrado de alae_batch_nominal
 
  DELETE FROM alae_batch_nominal
- WHERE alae_batch_nominal.fk_batch IN (347, 348, 349, 350, 351, 352, 358, 359, 360, 361, 362, 363, 417, 425);
+--  WHERE alae_batch_nominal.fk_batch IN (347, 348, 349, 350, 351, 352, 358, 359, 360, 361, 362, 363, 417, 425);
  
--- WHERE alae_batch_nominal.fk_batch IN (	SELECT pk_batch FROM alae_batch WHERE alae_batch.fk_study IN (397));
+ WHERE alae_batch_nominal.fk_batch IN (	SELECT pk_batch FROM alae_batch WHERE alae_batch.fk_study IN (403));
 
 -- Borrado d-- e alae_sample_Batch
  DELETE FROM alae_sample_batch
- WHERE alae_sample_batch.fk_batch IN (347, 348, 349, 350, 351, 352, 358, 359, 360, 361, 362, 363, 417, 425);
--- WHERE alae_sample_batch.fk_batch IN  (	SELECT pk_batch FROM alae_batch WHERE alae_batch.fk_study IN (397));
+ -- WHERE alae_sample_batch.fk_batch IN (347, 348, 349, 350, 351, 352, 358, 359, 360, 361, 362, 363, 417, 425);
+ WHERE alae_sample_batch.fk_batch IN  (	SELECT pk_batch FROM alae_batch WHERE alae_batch.fk_study IN (403));
 -- 
 -- Borrado de alae_batch
-DELETE FROM alae_batch WHERE pk_batch IN (8268,8269,);
--- DELETE FROM alae_batch WHERE alae_batch.fk_study IN (397);
+--DELETE FROM alae_batch WHERE pk_batch IN (8268,8269,);
+ DELETE FROM alae_batch WHERE alae_batch.fk_study IN (403);
 -- -- 
 -- -- -- Borrado de alae_sample_verification_study
 -- DELETE FROM alae_sample_verification_study WHERE alae_sample_verification_study.fk_analyte_study IN 
